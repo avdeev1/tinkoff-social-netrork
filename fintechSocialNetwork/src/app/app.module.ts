53b133e2c540e51727bd17e7424ba97eb77b1542
Avdey <avdeev.1van@icloud.com>
2019-03-29 22:36:24 +0300

    Initial commit

---
+++ fintechSocialNetwork/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NbThemeModule } from '@nebular/theme';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NbThemeModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
