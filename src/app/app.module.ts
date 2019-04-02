734595497caa45a442312aba452d900238cb388f
AlexandraSannikova <Sannikovaal@mail.ru>
2019-04-02 15:38:57 +0300

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NbThemeModule } from '@nebular/theme';
+import {NbButtonModule, NbInputModule, NbLayoutModule, NbThemeModule} from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NbThemeModule.forRoot()
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbButtonModule,
+    NbInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
