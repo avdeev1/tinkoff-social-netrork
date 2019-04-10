411597fe324606896f684ec9aaa443d8c6071f39
Danil <dehow@mail.ru>
2019-04-10 17:44:53 +0300

    update auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { ResolversModule } from './resolvers/resolvers.module';
-import { GuardsModule } from './guards/guards.module';
-import { AppLoginComponent } from './app-login/app-login.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,12 +15,10 @@ import { NbThemeModule, NbMenuModule, NbLayoutModule, NbCardModule, NbSidebarMod
 @NgModule({
   declarations: [
     AppComponent,
-    AppLoginComponent,
   ],
   imports: [
     FormsModule,
     RouterModule,
-    GuardsModule,
     BrowserModule,
     ResolversModule,
     HttpClientModule,
