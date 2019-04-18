4d020beba76aeba94e0c19023cb9397f37d185ed
Avdey <avdeev.1van@icloud.com>
2019-04-19 01:02:34 +0300

    fixed bugs

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,8 +7,9 @@ import { IUser } from '../models';
 export class UserService {
 
   public user: IUser = {
-    user: '@norimyxxxo',
-    profilePhotoSrc: 'https://pbs.twimg.com/profile_images/1029031247073476610/trKYjiJ3_400x400.jpg',
+    id: 1,
+    login: '@norimyxxxo',
+    avatar: 'https://pbs.twimg.com/profile_images/1029031247073476610/trKYjiJ3_400x400.jpg',
     postIds: [1, 4],
     description: 'русскоязычный рэп-, грайм-, фристайл-исполнитель и исполнительный директор букинг-агентства «Booking Machine Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda delectus eius enim illo inventore maxime minus nemo nisi non nostrum perspiciatis, porro possimus qui sequi soluta sunt vero voluptatem.'
   };
