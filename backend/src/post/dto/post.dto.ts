c90516cd6ed36f229ac214ebc13eb6b41eb745e3
Avdey <avdeev.1van@icloud.com>
2019-04-22 17:13:24 +0300

    mpdified postDto and create post and make getTags() more simple

--- backend/src/post/dto/post.dto.ts
+++ backend/src/post/dto/post.dto.ts
@@ -1,4 +1,4 @@
-import {IsNotEmpty, IsString} from "class-validator";
+import {IsNotEmpty, IsOptional, IsString} from "class-validator";
 
 export class PostDto {
   @IsNotEmpty()
@@ -9,6 +9,8 @@ export class PostDto {
   @IsString()
   text: string;
 
+  @IsString()
+  @IsOptional()
   image?: string;
 
   tags: number[];
